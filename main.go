a6493f37a5b243e321ff40c9f101d3e2c93f8f7b
filodoks <filodoks@gmail.com>
2019-12-11 23:37:17 +0100

    map function

--- main.go
+++ main.go
@@ -6,9 +6,14 @@ func main() {
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
-		"white": "#ffffff"
+		"white": "#ffffff",
 	}
 
+	printMap(colors)
+}
 
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
